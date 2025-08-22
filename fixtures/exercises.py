cafa33df2ffa77077d810deadbb2f889672a7830
afanasievkirill <afanasiev.kirill@gmail.com>
2025-08-22 10:05:26 +0300

    9.1

---
+++ fixtures/exercises.py
@@ -0,0 +1,29 @@
+import pytest
+from pydantic import BaseModel
+
+from clients.exercises.exercises_client import get_exercise_client, ExercisesClient
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from fixtures.courses import CourseFixture
+from fixtures.users import UserFixture
+
+
+class ExerciseFixture(BaseModel):
+    request: CreateExerciseRequestSchema
+    response: CreateExerciseResponseSchema
+
+
+@pytest.fixture
+def exercise_client(function_user: UserFixture) -> ExercisesClient:
+    return get_exercise_client(function_user.authentication_user)
+
+
+@pytest.fixture
+def function_course(
+        exrcises_client: ExercisesClient,
+        function_course: CourseFixture
+) -> ExerciseFixture:
+    request = CreateExerciseRequestSchema(
+        course_id = function_course.response.course.id
+    )
+    response = exrcises_client.create_course(request)
+    return ExerciseFixture(request=request, response=response)
